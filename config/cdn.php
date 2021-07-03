609b1255b54c3ba6154d0c97e09b156610b77a7f
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2021-07-03 13:49:44 +0200

    Add first config entries

--- config/cdn.php
+++ config/cdn.php
@@ -1,5 +1,45 @@
 <?php
-// config for Area17/ClassName
-return [
 
+use Illuminate\Http\Response;
+use A17\CDN\Support\Constants;
+use Illuminate\Http\JsonResponse;
+
+return [
+    'service' => A17\CDN\Services\Akamai\Service::class,
+
+    'cache-control' => ['max-age' => 1 * Constants::WEEK],
+
+    'responses' => [
+        'cachable' => [
+            Illuminate\Http\Response::class,
+            Illuminate\Http\JsonResponse::class,
+        ],
+
+        'not-cachable' => [],
+    ],
+
+    'routes' => [
+        'cachable' => [],
+
+        'not-cachable' => [
+            'pdf.ticket',
+            'awallet.ticket',
+            'gwallet.ticket',
+            'newsletter.store',
+            'newsletter',
+            'api.',
+        ],
+    ],
+
+    'methods' => [
+        'cachable' => ['GET'],
+
+        'not-cachable' => [],
+    ],
+
+    'statuses' => [
+        'cachable' => [200, 301],
+
+        'not-cachable' => [],
+    ],
 ];
