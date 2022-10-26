5bada44e7527edc2fff23d33708f71fcc7189e52
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2022-10-26 15:25:49 +0200

    Add an event listener to get all models invalidations directly from Laravel events

---
+++ src/Listeners/EloquentSaved.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace A17\EdgeFlush\Listeners;
+
+use A17\EdgeFlush\Behaviours\CachedOnCDN;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class EloquentSaved
+{
+    use CachedOnCDN;
+
+    public function handle(string $event, $models): void
+    {
+        foreach ($models as $model) {
+            $this->invalidateCDNCache($model);
+        }
+    }
+}
